225eb842b1b2f6ce86b2f78841de05f3927ca6bb
Mox Muriz <mox.muriz@hoelderlin.com>
2020-02-29 21:39:58 +0100

    Add emty component for selecting a todo list

---
+++ src/app/to-do-list-selection/to-do-list-selection.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'to-do-list-selection',
+  templateUrl: './to-do-list-selection.component.html',
+  styleUrls: ['./to-do-list-selection.component.css']
+})
+export class ToDoListSelectionComponent {
+
+  constructor() {
+  }
+
+}
