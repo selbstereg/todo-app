0c4b6e3bf52720914d59437d7d70c559643e1e54
Mox Muriz <mox.muriz@hoelderlin.com>
2020-03-01 18:02:04 +0100

    Deployment commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Run this via \"npm run deploy\". Otherwise ng won't be found."
 
-ng build --prod --output-path docs --base-href /todo-app/
+ng build --prod --source-map --output-path docs --base-href /todo-app/
 cp docs/index.html docs/404.html
 git add docs
 git commit -a -m 'Deployment commit'
