216530c2b66d8596dc98aff0a35fd97d0cd53387
Mox Muriz <mox.muriz@hoelderlin.com>
2020-03-01 12:14:22 +0100

    Show first to do list, when app starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NamedEntity } from './to-do-list/model/named-entity.model';
+import { HttpClient } from '@angular/common/http';
+import { TO_DO_LISTS_ENDPOINT_URL } from './common/constants';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly faBars = faBars;
   private sideNavOpened = false;
   private selectedToDoList: NamedEntity = null;
+  
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get(TO_DO_LISTS_ENDPOINT_URL).pipe(
+      take(1)
+    ).subscribe((body: NamedEntity[]) => this.selectedToDoList = body[0]);
+  }
 
   openSideNav() {
     this.sideNavOpened = true;
