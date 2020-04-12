1464a5e24331b35ef0825286f7bffe6ee519c5d8
Mox Muriz <mox.muriz@hoelderlin.com>
2020-04-12 16:34:50 +0200

    Change app styles

--- src/app/common/delete-button/delete-button.component.ts
+++ src/app/common/delete-button/delete-button.component.ts
@@ -3,6 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'delete-button',
   templateUrl: './delete-button.component.html',
+  styleUrls: ['./delete-button.component.css']
 })
 export class DeleteButtonComponent {
   @Output() delete = new EventEmitter<void>();
